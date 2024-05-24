41065d751dadbd1af7a286fe1d15d202028192c2
Anezz12 <harsenaargrtya1@gmail.com>
2024-05-24 14:55:36 +0700

    first-commit

---
+++ src/components/header.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const navLinks = [
+  {
+    href: "/",
+    label: "Home",
+  },
+  {
+    href: "/posts",
+    label: "Posts",
+  },
+];
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center py-4 px-7 border-b">
+      <Image
+        src="https://bytegrad.com/course-assets/youtube/example-logo.png"
+        alt="Logo"
+        className="w-[35px] h-[35px]"
+        width="35"
+        height="35"
+      />
+      <nav>
+        <ul className="flex gap-x-5 text-[14px] font-bold">
+          {navLinks.map((link) => (
+            <li key={link.href}>
+              <Link className=" text-zinc-400" href={link.href}>
+                {link.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
